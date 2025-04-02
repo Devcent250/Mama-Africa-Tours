820cc1a2e9c4814a35e9a01d3e53d6933881cfed
Devcent250 <dusinnocent57@gmail.com>
2025-04-02 23:51:39 +0200

    last commit

--- src/Components/shared/Header.jsx
+++ src/Components/shared/Header.jsx
@@ -3,19 +3,22 @@ import { FaPhone, FaEnvelope } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 left-0 w-full bg-green-600 text-white py-2 z-50 font-coolvetica font-normal">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
-        <div className="flex items-center space-x-6 text-sm">
-          <span className="flex items-center">
-            <FaPhone className="mr-2" /> +250 788 784 023 906
-          </span>
-          <span className="flex items-center">
-            <FaEnvelope className="mr-2" /> mamafricatour1@gmail.com
-          </span>
-        </div>
+    <div className="fixed top-0 left-0 w-full bg-white text-green-500 z-50 font-coolvetica shadow-md overflow-hidden">
+      <div className="max-w-screen-lg mx-auto flex flex-wrap justify-between items-center w-full px-2 py-2 font-semibold text-xs md:text-sm">
+
+
+        <span className="flex items-center whitespace-nowrap">
+          <FaEnvelope className="text-sm mr-1" /> mamafricatour1@gmail.com
+        </span>
+
+
+        <span className="flex items-center whitespace-nowrap">
+          <FaPhone className="text-sm mr-1" /> +250 788 784 023 906
+        </span>
       </div>
     </div>
   );
 };
+
 
 export default Header;
